5484945b92db317964d5f4df6096b33f03811f66
sfruano <sergio.fernandezruano@unican.es>
2022-03-25 15:30:49 +0100

    Added Glauert Correction

---
+++ src/glauert.py
@@ -0,0 +1,15 @@
+
+from numpy  import ndarray, sqrt, zeros
+
+
+def glauert_correction(ct: ndarray) -> ndarray:
+    ct1 = 1.816
+    ct2 = 2*sqrt(ct1)-ct1
+
+    a = zeros(ct.shape)
+    pos = ct < ct2
+    a[pos] = 0.5 - sqrt(1-ct[pos])/2.0
+    pos = ct >= ct2
+    a[pos] = 1+(ct[pos]-ct1)/4.0/(sqrt(ct1)-1.0)
+
+    return a
