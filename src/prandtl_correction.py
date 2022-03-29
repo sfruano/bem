54a9638893ff1e6fb1b5cc1cb39bc061bc223566
Harika Gurram <hgurram@tacc.utexas.edu>
2022-03-29 09:20:57 -0500

    Update prandtl_correction.py

--- src/prandtl_correction.py
+++ src/prandtl_correction.py
@@ -1,13 +1,13 @@
 
 import numpy as np  
 
-def Prandtl_corrections(ratio, Rroot, lamda, nblades, a)
+def Prandtl_corrections(ratio, mu_root, lamda, nblades, a)
 
 """"
 r = radial position of the blade
 R = Turbine radius
-ratio = r/R
-Rroot = Root radius
+ratio = r/R (mu)
+mu_root = Root radius
 lamda = Tip speed ratio
 nblades = Number of blades 
 a = Axial induction 
@@ -17,7 +17,7 @@ a = Axial induction
 num1 = -nblades/2*(1-ratio)/ratio*np.sqrt( 1+ ((lamda*ratio)**2)/((1-a)**2))
 Ftip = np.array(2/np.pi*np.arccos(np.exp(num1)))
 
-num2 = -nblades/2*(ratio-Rroot)/ratio*np.sqrt( 1+ ((lamda*ratio)**2)/((1-a)**2))
+num2 = -nblades/2*(ratio-mu_root)/ratio*np.sqrt( 1+ ((lamda*ratio)**2)/((1-a)**2))
 Froot = np.array(2/np.pi*np.arccos(np.exp(num2)))
 
 p_lim = 0.0001
